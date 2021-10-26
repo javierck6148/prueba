16322585d008197fcba1c5077765942c7d7b91b5
JavierRL <javier.rodriguezl@alumno.buap.mx>
2021-10-26 14:02:20 -0500

    Tutorial completado hasta antes de prop types

--- myfistapp/src/App.js
+++ myfistapp/src/App.js
@@ -1,48 +1,20 @@
 import './App.css';
 import React from 'react';
+import tasks from './sample/task.json';
+import Tasks from './components/Tasks';
 
-class HelloWorld extends React.Component
-{
 
-  state ={
-    show:true  
-  }
+class App extends React.Component {
   
-  cambiarMostrar = () => this.setState({show: !this.state.show})
-  
-
-
-  render(){ 
-    if(this.state.show){  
-      return(
-      
-      <div  id = "hello">
-        <h3>{this.props.subtitle}</h3>
-        {this.props.mytext}
-        <button onClick={this.cambiarMostrar }>Cambiar mostrar</button>
-      </div>
-      );
-    }else{
-      return(
-        <h1> No hay componente
-          <button onClick={this.cambiarMostrar}> a </button>
-        </h1>
-      );
-    }
+  state = {
+    tasks:tasks
   }
 
-}
-
-
-
-function App() {
-  return (
-    <div >This is my component: 
-      <HelloWorld mytext="Hello World" subtitle="lorem ipsum"/> 
-      <HelloWorld mytext="HOLA MUNDO" subtitle="shalala shalala"/> 
-      <HelloWorld mytext="Ohaio Sekai" subtitle="darken zoutto"/> 
-      </div>
-  );
+  render(){
+    return <div>
+      <Tasks tasks ={this.state.tasks} />
+    </div>
+  }
 }
 
 export default App;
